d90d64a4409952ee309c42cdc99c895202cefa5e
Dominik Inführ <dominik.infuehr@gmail.com>
2013-07-28 07:13:59 +0200

    Players can be created and deleted

--- app/app.js
+++ app/app.js
@@ -1,5 +1,53 @@
 var gameApp = angular.module( 'herzeln', [] );
 
+gameApp.factory( 'playerService', function( ) {
+  var service = {};
+  var players = [];
+
+  if( window.localStorage ) {
+    var tmp;
+
+    try {
+      tmp = JSON.parse( window.localStorage.players );
+    } catch( e ) {
+      tmp = null;
+    }
+
+    if( angular.isArray( tmp ) ) {
+      players = tmp;
+      console.log( players );
+    }
+  }
+
+  function playersChanged( ) {
+    window.localStorage.players = JSON.stringify( players );
+  }
+
+  service.getPlayers = function( ) {
+    return players;
+  }
+
+  service.addPlayer = function( name ) {
+    if( _.contains( players, name ) ) {
+      return false;
+
+    } else {
+      players.push( name );
+      return true;
+    }
+  }
+
+  service.removePlayer = function( name ) {
+    var ind = players.indexOf( name );
+
+    if( ind >= 0 ) {
+      players.splice( ind, 1 );
+    }
+  }
+
+  return service;
+} );
+
 gameApp.config( function( $routeProvider ) {
   $routeProvider.when( '/', {
     templateUrl: 'app/player.html',
@@ -14,17 +62,30 @@ gameApp.config( function( $routeProvider ) {
   $routeProvider.otherwise( { redirectTo: '/' } );
 } );
 
-function PlayerCtrl( $scope ) {
-  $scope.players = [];
+function PlayerCtrl( $scope, playerService ) {
+  $scope.players = playerService.getPlayers( );
 
-  $scope.addPlayer = function( name ) {
-    $scope.players.push( $scope.name );
-    $scope.name = "";
+  $scope.addPlayer = function( ) {
+    if( playerService.addPlayer( $scope.name ) ) {
+      $scope.name = "";
+    } else {
+      console.log( "error" );
+    }
+  }
+
+  $scope.removePlayer = function( name ) {
+    playerService.removePlayer( name );
+    console.log( "delete name = " + name );
   }
 }
 
-function GameCtrl( $scope ) {
+function GameCtrl( $scope, playerService ) {
+  $scope.games = [];
+  $scope.players = playerService.getPlayers( );
 
+  $scope.addGame = function( ) {
+    $scope.games.push( "hello" );
+  }
 }
 
 gameApp.controller( 'PlayerCtrl', PlayerCtrl );
