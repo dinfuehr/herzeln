d6a9b9730eae3ded84cc11ff93f3e15de8779743
Dominik Inführ <dominik.infuehr@gmail.com>
2013-07-31 22:43:38 +0200

    Begonnen zweite Maske fertig zu stellen

--- app/app.js
+++ app/app.js
@@ -75,12 +75,75 @@ function PlayerCtrl( $scope, playerService ) {
   }
 }
 
+function cardsPerTypeFromPlayers( players ) {
+  var cards = 8;
+
+  var types = 4;
+  var maxPerType = 13;
+
+  var max = maxPerType * types; // 52 Karten hat ein Deck
+
+  while( cards * players > max ) {
+    cards--;
+  }
+
+  return cards;
+}
+
+function totalPoints( cardsPerType ) {
+  return cardsPerType * 2 + // Stich + Herz
+    8 + 8 + 8; // Buben + rote Damen + Herz Koenig
+}
+
+function Game( players ) {
+  var rounds = [ 'stich', 'herz', 'buben', 'damen', 'koenig', 'anlegen' ];
+  var _this = this;
+
+  _.each( rounds, function( round ) {
+    var points = {};
+
+    _.each( players, function( player ) {
+      points[ player ] = 0;
+    } );
+
+    _this[ round ] = points;
+  } );
+}
+
 function GameCtrl( $scope, playerService ) {
   $scope.games = [];
   $scope.players = playerService.getPlayers( );
+  $scope.cardsPerType = cardsPerTypeFromPlayers( $scope.players );
+  $scope.totalCards = $scope.cardsPerType * 4;
+  $scope.totalPoints = totalPoints( $scope.cardsPerType );
+
+  $scope.rounds = [
+    { round: 'stich', name: 'Stich' },
+    { round: 'herz', name: 'Herz' },
+    { round: 'buben', name: 'Buben' },
+    { round: 'damen', name: 'Damen' },
+    { round: 'koenig', name: 'Koenig' },
+    { round: 'anlegen', name: 'Auslegen' }
+  ];
+
+  $scope.points = [ ];
+
+  _.each( $scope.players, function( ) {
+    $scope.points.push( { points: 0 } );
+  } );
 
   $scope.addGame = function( ) {
-    $scope.games.push( "hello" );
+    $scope.games.push( new Game( $scope.players ) );
+  }
+
+  $scope.restPoints = function( ) {
+    var sum = 0;
+
+    _.each( $scope.points, function( point ) {
+      sum += point.points;
+    } );
+
+    return $scope.totalPoints - sum;
   }
 }
 
